52fa2aee051cb304859f3f085b19664c067402c7
JEdilbertoTC <joseedilberto367@outlook.com>
2020-10-01 13:13:53 -0500

    Impletacion checkbox, editbox, listbox

--- src/uaslp/enginering/labs/Checkbox.java
+++ src/uaslp/enginering/labs/Checkbox.java
@@ -1,4 +1,14 @@
 package uaslp.enginering.labs;
+import java.util.List;
 
-public class Checkbox {
+public class Checkbox extends Control{
+    String label;
+    List<Control> checkbox;
+
+    public Checkbox(String label){
+        this.label = label;
+    }
+    public void draw(){
+        System.out.print("[ ]"+ label);
+    }
 }
--- src/uaslp/enginering/labs/ListBox.java
+++ src/uaslp/enginering/labs/ListBox.java
@@ -1,4 +1,20 @@
 package uaslp.enginering.labs;
 
-public class ListBox {
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class ListBox extends Control{
+    List<String> options;
+    public ListBox(String ... options){
+        this.options = new ArrayList<>(options.length);
+        Collections.addAll(this.options,options);
+    }
+
+    protected void draw(){
+        for (String option : options){
+            System.out.print(option + " -");
+        }
+    }
 }
